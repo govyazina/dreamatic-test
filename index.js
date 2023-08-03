b99ccc588c13f5b3c1b80b19dfbf573f50f57ba5
Svetlana Govyazina <govyazina@gmail.com>
2023-08-03 16:55:00 +0300

    amount conversion added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
     getListCurrencyTypes()
+    let rate = 0
     document.querySelector('#currency_type_first').addEventListener('change', onChange)
     document.querySelector('#currency_type_second').addEventListener('change', onChange)
     const currencyRates = await getCurrencyRates()
     document.querySelector('.rate_data').innerHTML = currencyRates.date
+    document.querySelector('#value_first').addEventListener('input', () => onInput(1))
+    document.querySelector('#value_second').addEventListener('input', () => onInput(2))
 
     async function getListCurrencyTypes() {
         const url = new URL("http://data.fixer.io/api/symbols");
@@ -33,23 +36,33 @@ document.addEventListener('DOMContentLoaded', async () => {
         getExchangeRate(firstCurrencyType, secondCurrencyType)
     }
 
+    function onInput(i) {
+        const firstValue = document.querySelector('#value_first').value
+        const secondValue = document.querySelector('#value_second').value
+        if (i === 1) {
+            document.querySelector('#value_second').value = (firstValue * rate).toFixed(2)
+        }
+        else {
+            document.querySelector('#value_first').value = (secondValue * rate).toFixed(2)
+        }
+    }
+
 
     async function getExchangeRate(firstCurrencyType, secondCurrencyType) {
-        let rate = 0
         if (firstCurrencyType === 'EUR') {
-            rate = currencyRates.rates[secondCurrencyType]
+            rate = (currencyRates.rates[secondCurrencyType]).toFixed(4)
         }
         else if (secondCurrencyType === 'EUR') {
-            rate = 1 / currencyRates.rates[firstCurrencyType]
+            rate = (1 / currencyRates.rates[firstCurrencyType]).toFixed(4)
         }
         else {
             const rate1 = currencyRates.rates[firstCurrencyType]
             const rate2 = currencyRates.rates[secondCurrencyType]
-            rate = (1 / rate1) * rate2
+            rate = ((1 / rate1) * rate2).toFixed(4)
         }
         document.querySelector('.first_currency').innerHTML = firstCurrencyType
         document.querySelector('.second_currency').innerHTML = secondCurrencyType
-        document.querySelector('.second_currency_rate').innerHTML = rate
+        document.querySelector('.second_currency_rate').innerHTML = Number(rate).toFixed(2)
     }
 });
 
