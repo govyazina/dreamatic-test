9ee2e21efab5de966a2deb4a024856f640b35220
Svetlana Govyazina <govyazina@gmail.com>
2023-08-03 16:00:58 +0300

    exchange for EUR case added

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// http://data.fixer.io/api/latest?access_key=74862ddc35eb9cd50dab455a68c6b8c3
-document.addEventListener('DOMContentLoaded', getListCurrencyTypes)
-async function getListCurrencyTypes() {
-    const url = new URL("http://data.fixer.io/api/symbols");
-    url.searchParams.set("access_key", '74862ddc35eb9cd50dab455a68c6b8c3');
-    let currencyList = await fetch(url)
-        .then(response => response.json())
-        .then(result => result.symbols);
-    let stringOptions = ''
-    for (const currency in currencyList) {
-        stringOptions += `<option value="${currency}">${currencyList[currency]}</option>`
+document.addEventListener('DOMContentLoaded', async () => {
+    getListCurrencyTypes()
+    document.querySelector('#currency_type_first').addEventListener('change', onChange)
+    document.querySelector('#currency_type_second').addEventListener('change', onChange)
+    const currencyRates = await getCurrencyRates()
+    document.querySelector('.rate_data').innerHTML = currencyRates.date
+
+    async function getListCurrencyTypes() {
+        const url = new URL("http://data.fixer.io/api/symbols");
+        url.searchParams.set("access_key", '74862ddc35eb9cd50dab455a68c6b8c3');
+        let currencyList = await fetch(url)
+            .then(response => response.json())
+            .then(result => result.symbols);
+        let stringOptions = '';
+        for (const currency in currencyList) {
+            stringOptions += `<option value="${currency}">${currencyList[currency]}</option>`
+        }
+        document.querySelector('#currency_type_first').innerHTML = stringOptions;
+        document.querySelector('#currency_type_second').innerHTML = stringOptions;
     }
-    document.querySelector('#currency_type_first').innerHTML = stringOptions;
-    document.querySelector('#currency_type_second').innerHTML = stringOptions;
-}
+
+    async function getCurrencyRates() {
+        const url = new URL("http://data.fixer.io/api/latest");
+        url.searchParams.set("access_key", '74862ddc35eb9cd50dab455a68c6b8c3');
+        return await fetch(url)
+            .then(response => response.json())
+            .then(result => result);
+    }
+
+    function onChange() {
+        const firstCurrencyType = document.querySelector('#currency_type_first').value
+        const secondCurrencyType = document.querySelector('#currency_type_second').value
+        getExchangeRate(firstCurrencyType, secondCurrencyType)
+    }
+
+
+    async function getExchangeRate(firstCurrencyType, secondCurrencyType) {
+        let rate = 0
+        if (firstCurrencyType === 'EUR') {
+            rate = currencyRates.rates[secondCurrencyType]
+        }
+        if (secondCurrencyType === 'EUR') {
+            rate = 1 / currencyRates.rates[firstCurrencyType]
+        }
+        document.querySelector('.first_currency').innerHTML = firstCurrencyType
+        document.querySelector('.second_currency').innerHTML = secondCurrencyType
+        document.querySelector('.second_currency_rate').innerHTML = rate
+    }
+});
+
